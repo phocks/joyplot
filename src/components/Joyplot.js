a08f6873297c5b52ecf8660a0adc753426f3a104
Joshua Byrd <phocks@gmail.com>
2017-10-06 16:51:20 +1000

    The joys of charting again!

--- src/components/Joyplot.js
+++ src/components/Joyplot.js
@@ -5,10 +5,30 @@ import * as d3 from "d3";
 class Joyplot extends Component {
   constructor(props) {
     super(props);
-    // this.createChart = this.createChart.bind(this); // not needed?
+    this.state = {
+      width: 960,
+      height: 500
+    };
+    this.createChart = this.createChart.bind(this); // Bind to access within method
   }
   componentWillMount() {
-    // this.loadData();
+    // Set up a date parser
+    this.parseDate = d3.timeParse("%d/%m/%Y");
+
+    // set the range scales
+    this.xScale = d3.scaleTime().range([0, this.state.width]);
+    this.yScale = d3.scaleLinear().range([this.state.height, 0]);
+
+    // define the area
+    this.area = d3
+      .area()
+      .x(d => {
+        return x(d.date);
+      })
+      .y0(this.state.height)
+      .y1(d => {
+        return y(d.close);
+      });
   }
 
   componentDidMount() {
@@ -19,9 +39,29 @@ class Joyplot extends Component {
     this.loadData();
   }
 
-  createChart(error, file1) {
-    console.log(file1);
-    const svg = d3.select("svg");
+  createChart(error, dataFlat) {
+    // console.log(data[0].Week);
+    // console.log(this.parseDate(data[0].Week));
+    // console.log(this.state.width);
+
+    console.log(dataFlat);
+    // dataFlat.forEach(d => {
+    //   d.Week = this.parseDate(d["Week"]);
+    //   console.log(d["Week"]);
+    //   // d.close = +d.close;
+    // });
+
+    var data = d3
+      .nest()
+      .key(function(d) {
+        return d.activity;
+      })
+      .entries(dataFlat);
+
+    const svg = d3
+      .select("svg")
+      .attr("width", this.state.width)
+      .attr("height", this.state.height);
   }
 
   loadData() {
